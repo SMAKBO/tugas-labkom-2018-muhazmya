7ee2d01d594c85b670b11e21961472cc31225095
Xeverse <35517183+muhazmya@users.noreply.github.com>
2018-02-25 23:05:29 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $(window).scroll(function(e) {
+        var s = $(window).scrollTop(),
+            opacityVal = (s / 200);
+
+        $('.blurred-image').css('opacity', opacityVal);
+    });
+});
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
