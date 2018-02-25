6f2bdef38373758551610c482749bf0f75761c07
Xeverse <35517183+muhazmya@users.noreply.github.com>
2018-02-26 00:56:29 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,3 +20,16 @@ $(document).ready(function(){
         return false; 
     }); 
 });
+
+function clickCounter() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = "&nbsp;&nbsp;&nbsp;Liked " + localStorage.clickcount + " x";
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
