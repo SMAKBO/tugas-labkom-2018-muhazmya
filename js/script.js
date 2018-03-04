0d24ade44bf67dba19cc30ecb545eef844f79535
Xeverse <35517183+muhazmya@users.noreply.github.com>
2018-03-04 16:57:45 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -28,8 +28,42 @@ function clickCounter() {
         } else {
             localStorage.clickcount = 1;
         }
-        document.getElementById("result").innerHTML = "&nbsp;&nbsp;&nbsp;Liked " + localStorage.clickcount + " x";
+        document.getElementById("result").innerHTML = localStorage.clickcount ;
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
     }
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
+
+
+
+
+
+
+
