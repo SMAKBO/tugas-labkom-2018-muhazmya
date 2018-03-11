5aa378f0343b61c72ae2ef425439f6b0b2ab6908
Xeverse <35517183+muhazmya@users.noreply.github.com>
2018-03-11 06:27:52 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -61,6 +61,38 @@ function showDivs(n) {
   dots[slideIndex-1].className += " w3-white";
 }
 
+var slideIndex = 1;
+showDivs2(slideIndex);
+
+function plusDivs2(n) {
+  showDivs2(slideIndex += n);
+}
+
+function currentDiv2(n) {
+  showDivs2(slideIndex = n);
+}
+
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("demo2");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
+
+
+
+
+
+
 
 
 
